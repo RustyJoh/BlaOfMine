0ed09712f50c1b1834107acf757ab17dda343268
ozeri2018 <ozeri2018@gmail.com>
2018-07-16 10:26:37 -0400

    responsive

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World')
+document.getElementsByClassName("button-background")[0].addEventListener("change", backgroundImage, false);
+
+function backgroundImage (e) {
+  document.getElementsByClassName('button-background').input[0].files
+}
