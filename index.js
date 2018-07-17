40303032854dbfda05b0dfb1c3e6ed0f4a1859e3
ozeri2018 <ozeri2018@gmail.com>
2018-07-17 10:57:29 -0400

    text button

--- index.js
+++ index.js
@@ -28,3 +28,24 @@ function loadThemeImage(e) {
     }
   }
 }
+document.getElementsByClassName("button-logo")[0].addEventListener("change", loadLogoImage, false);
+function loadLogoImage(e) {
+  const files = document.getElementsByClassName('button-logo')[0].children[0].files
+  if (files.length > 0) {
+    let img = document.createElement("img");
+    img.src = window.URL.createObjectURL(files[0]);
+    img.height = 100;
+    img.width = 100;
+    img.onload = function() {
+      window.URL.revokeObjectURL(this.src);
+      document.getElementsByClassName("post-logo")[0].appendChild(img);
+   
+    }
+  }
+}
+document.getElementsByClassName("button-text")[0].addEventListener("change", textInput, false);
+function textInput(e){
+  
+  document.getElementsByClassName("post-text")[0].appendChild(input);
+  
+}
